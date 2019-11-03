240975f9465871a155620c1956a4846f292a08a3
BasimAhmedKhan <50414240+BasimAhmedKhan@users.noreply.github.com>
2019-11-03 21:11:12 +0500

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,10 @@
+print(" ")
+print("AREA OF CIRCLE")
+print(" ")
+
+rad = float(input('"Enter Radius": '))
+area = (rad**2 * 3.1415926535897932384626433832795)
+
+print(" ")
+print('Area Of Circle: ' + str(area))
+print(" ")
